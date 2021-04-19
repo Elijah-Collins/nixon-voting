2e1ea9e5a986494c895ae0afa32407bd5effc96c
Elijah Collins <ecollins152@west-mec.org>
2021-04-19 15:28:22 -0700

    toggle works

--- nixonVmcgovern.js
+++ nixonVmcgovern.js
@@ -1,3 +1,8 @@
-function cursorTrigger(){
-    cursor = url("../nixon-voting/cursorTest.jpeg"), pointer;
+function votingToggle(){
+    var body = document.getElementById("body");
+    if(body.classList.contains("toggledCursor")){
+        body.classList.remove("toggledCursor");
+    }else{
+        body.classList.add("toggledCursor");
+    }
 }

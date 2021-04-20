74a86e0a9aaf3f45d379b572265d299979bc6479
Elijah Collins <ecollins152@west-mec.org>
2021-04-20 14:33:56 -0700

    the basis of the page works

--- nixonVmcgovern.js
+++ nixonVmcgovern.js
@@ -6,3 +6,29 @@ function votingToggle(){
         body.classList.add("toggledCursor");
     }
 }
+function submitVoteNixon(){
+    var ballotBoxes = document.getElementsByClassName("ballotBox");
+    var body = document.getElementById("body");
+    var nixonBox = document.getElementById("Nixon_Ballot");
+    
+    if(body.classList.contains("toggledCursor")){
+        if(ballotBoxes = nixonBox){
+            alert("Thank you for voting for President Richard Nixon.");
+        }
+    }else{
+        alert("Please pick up a ballot and continue.");
+    }
+}
+function submitVoteMcGovern(){
+    var ballotBoxes = document.getElementsByClassName("ballotBox");
+    var body = document.getElementById("body");
+    var mcgovernBox = document.getElementById("McGovern_Ballot");
+    
+    if(body.classList.contains("toggledCursor")){
+        if(ballotBoxes = mcgovernBox){
+            alert("This ballot is closed at this time, please try again later.");
+        }
+    }else{
+        alert("Please pick up a ballot and continue.");
+    }
+}
